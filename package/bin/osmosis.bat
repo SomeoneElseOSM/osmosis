79b874bbdc3363d0a12d805f5f437fd67cf71d4b
Andy Townsend <ajtown@atownsend.org.uk>
2018-01-31 22:51:44 +0000

    Added back my change to the Windows "osmosis.bat" so that it's more likely to run on Windows.

--- package/bin/osmosis.bat
+++ package/bin/osmosis.bat
@@ -1,29 +1,29 @@
 @ECHO OFF
 
-REM This is an equivalent Windows batch file to complement the unix shell script
-REM Corresponding lines from the shell script are printed before the matching batch file commands
+rem ------------------------------------------------------------------------------
+rem This was developed from the previous Windows "osmosis.bat" which in turn was 
+rem a copy of the Unix version.
+rem
+rem The main changes are to add comments and remove things that can break, so 
+rem no execution of random batch files from unexpected folders such as 
+rem "C:\ProgramData"
+rem ------------------------------------------------------------------------------
 
-REM # Config files can define several variables used throughout this script.
-REM # JAVACMD - The java command to launch osmosis.
-REM # JAVACMD_OPTIONS - The options to append to the java command, typically used to modify jvm settings such as max memory.
-REM # OSMOSIS_OPTIONS - The options to apply to all osmosis invocations, typically used to add plugins or make quiet operation the default.
+rem ------------------------------------------------------------------------------
+rem Use "java" from the path.  This is what almost every other Java application
+rem will do; anything else will just confuse users.
+rem ------------------------------------------------------------------------------
+set JAVACMD=java
 
-REM if [ -f /etc/osmosis ] ; then
-REM   . /etc/osmosis
-REM fi
-IF EXIST "%ALLUSERSPROFILE%\osmosis.bat" CALL "%ALLUSERSPROFILE%\osmosis.bat"
-
-REM if [ -f "$HOME/.osmosis" ] ; then
-REM   . "$HOME/.osmosis"
-REM fi
-IF EXIST "%USERPROFILE%\osmosis.bat" CALL "%USERPROFILE%\osmosis.bat"
-
-
-REM if [ -z "$JAVACMD" ] ; then
-REM   # No JAVACMD provided in osmosis config files, therefore default to java
-REM   JAVACMD=java
-REM fi
-IF "%JAVACMD%"=="" set JAVACMD=java
+rem ------------------------------------------------------------------------------
+rem The net result of the jiggerypokery below is so that a command similar to this
+rem eventually gets executed:
+rem
+rem "java" -cp "C:\Temp\osmosis-latest\lib\default\plexus-classworlds-2.5.2.jar" -Dapp.home="C:\Temp\osmosis-latest" -Dclassworlds.conf="C:\Temp\osmosis-latest\config\plexus.conf" org.codehaus.classworlds.Launcher
+rem
+rem with the full path used for the "classworlds" launcher, "app_home" and the 
+rem classworlds.conf file.
+rem ------------------------------------------------------------------------------
 
 REM Set "SAVEDIR" to the current directory
 set SAVEDIR=%CD%
@@ -36,6 +36,6 @@ cd /D %SAVEDIR%
 
 set MAINCLASS=org.codehaus.classworlds.Launcher
 set PLEXUS_CP=%MYAPP_HOME%\lib\default\plexus-classworlds-2.5.2.jar
-SET EXEC="%JAVACMD%" %JAVACMD_OPTIONS% -cp "%PLEXUS_CP%" -Dapp.home="%MYAPP_HOME%" -Dclassworlds.conf="%MYAPP_HOME%\config\plexus.conf" %MAINCLASS%  %OSMOSIS_OPTIONS% %*
+SET EXEC="%JAVACMD%" -cp "%PLEXUS_CP%" -Dapp.home="%MYAPP_HOME%" -Dclassworlds.conf="%MYAPP_HOME%\config\plexus.conf" %MAINCLASS%  %OSMOSIS_OPTIONS% %*
 
 %EXEC%
